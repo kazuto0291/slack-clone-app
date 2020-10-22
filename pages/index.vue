78d4ef7dacdcb3804e39f48808f14d08818a6147
kazuto0291 <zutozuto03170291@gmail.com>
2020-10-22 22:18:31 +0900

    ログイン機能実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <button v-on:click="login">ログイン</button>
   </div>
 </template>
 
@@ -10,17 +9,7 @@ import { firebase } from '~/plugins/firebase'
 
 export default {
   methods: {
-    login() {
-      window.alert('ログインするよ')
-      const provider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-          const user = result.user
-          console.log(user)
-        }).catch((error) => {
-          window.alert(error)
-        })
-    }
+  
   }
 }
 </script>
